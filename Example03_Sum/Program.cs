4b833c16a2c9d72ca8a4111297d2b2c3090b69d2
«Muzhait» <muzhait@mail.ru>
2023-01-25 10:23:15 +0500

    сhange sum

--- Example03_Sum/Program.cs
+++ Example03_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 317;
-int numberB = 483;
+﻿int numberA = new Random().Next(15, 47);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 15);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
