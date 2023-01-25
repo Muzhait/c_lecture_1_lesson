9703c376f2dd8de7187a202688a5f323d0e29733
«Muzhait» <muzhait@mail.ru>
2023-01-25 10:41:01 +0500

    add if else

---
+++ Example05_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите свое имя о драгоценнейщий пользователь: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "карим")
+{
+    Console.WriteLine("Добро пожаловать о великий Карим, достойнейщий из достойных, свет и надежда нашего государства!");
+}
+else
+{
+    Console.Write("Ну заходи раз пришел, ");
+    Console.WriteLine(username);
+}
